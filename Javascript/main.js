a6e2fa0d53330ca0443abd51c7934f609286b816
Cass Valladares <jhumphriesent@gmail.com>
2020-08-14 16:01:49 -0500

    first commit

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,54 +1,142 @@
+while (!player1){
+    var player1 = prompt('Player One: Enter your name. You will be GOKU.');
+};
+var player1Color = 'orange';
+
+while (!player2){
+    var player2 = prompt('Player Two: Enter your name. You will be VEGETA.');
+};
+var player2Color = 'blue';
+
+
 var tableRow = document.getElementsByTagName('tr');
-var tableCell = document.getElementsByTagName('td');
-var tableSlot = document.querySelector('.slot');
-const playerTurn = document.querySelector('.player-turn');
-const restart = document.querySelector('.restart');
+var tableData = document.getElementsByTagName('td');
+var playerTurn = document.querySelector('.player-turn');
+const spots = document.querySelectorAll('.spot');
+const restartBtn = document.querySelector('.restart');
 
-for(let i = 0; i < tableCell.length; i++){
-    tableCell[i].addEventListener('click', (e) =>{
-        console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`);
-    })
-}
-while(!player1){
-    var player1 = prompt('Youre Goku! Enter your name');
-}
-
-player1Img = "Images/Vegeta.png";
-
-
-
-while(!player2){
-    var player2 = prompt('Youre Vegeta! Enter your name');
-}
-
-player2Img = "Images/Goku.png";
 
 var currentPlayer = 1;
-playerTurn.textContent = `${player1}'s turn!`;
+let winner;
+playerTurn.textContent = `${player1}'s turn!`
 
-Array.prototype. forEach.call(tableCell, (cell) =>{
-    cell.addEventListener('click', changeCharacter);
-    cell.style.backgroundColor ='white';
-})
+for (i = 0; i < tableData.length; i ++){
+    tableData[i].addEventListener('click', (e) =>{
+        console.log(`${e.target.parentElement.rowIndex},${e.target.cellIndex}`)
+    });
+};
 
-function changeCharacter(e){
+
+function changeColor(e){
     let column = e.target.cellIndex;
-    let row =[];
+    let row = [];
 
-    for(let i =5; i > -1; i--){
-        if(tableRow[i].children[column].style.backgroundColor == 'white'){
+    for (i = 5; i > -1; i--){
+        if (tableRow[i].children[column].style.backgroundColor == 'white'){
             row.push(tableRow[i].children[column]);
-            if(currentPlayer === 1){
-                row[0].style.backgroundImage = url(player1Img);
-                playerTurn.textContent = `${player2}'s turn!`;
-                return currentPlayer = 2;
+            if (currentPlayer === 1){
+                row[0].style.backgroundColor = 'orange';
+                if (horizontalCheck() || verticalCheck() || diagonalCheck() || diagonalCheck2()){
+                    playerTurn.textContent = `${player1} IS THE WINNER!!`;
+                    playerTurn.style.color = player1Color;
+                    return alert(`${player1} IS THE WINNER!!`);
+                }else if (drawCheck()){
+                    playerTurn.textContent = 'ITS A TIE';
+                    return alert('ITS A TIE!');
+                }else{
+                    playerTurn.textContent = `${player2}'s TURN!`
+                    return currentPlayer = 2;
+                }
             }else{
-                row[0].style.backgroundImage = url(player2Img);
-                playerTurn.textContent = `${player1}'s turn!`
-                return currentPlayer = 1;
+                row[0].style.backgroundColor = 'blue';
+                if (horizontalCheck() || verticalCheck() || diagonalCheck() || diagonalCheck2()){
+                    playerTurn.textContent = `${player2} WINS!!`;
+                    playerTurn.style.color = player2Color;
+                    return alert(`${player2} WINS!!`);
+                }else if (drawCheck()){
+                    playerTurn.textContent = 'ITS A TIE!';
+                    return alert('ITS A TIE!');
+                }else{
+                    playerTurn.textContent = `${player1}`;
+                    return currentPlayer = 1;
+                }
+                
             }
         }
+    }
+   
+}
 
+Array.prototype.forEach.call(tableData, (cell) => {
+    cell.addEventListener('click', changeColor);
+    cell.style.backgroundColor = 'white';
+});
+
+function colorMatchCheck(one, two, three, four){
+    return (one === two && one === three && one === four && one !== 'white' && one !== undefined);
+}
+
+function horizontalCheck(){
+    for (let row = 0; row < tableRow.length; row++){
+        for (let col =0; col < 4; col++){
+           if (colorMatchCheck(tableRow[row].children[col].style.backgroundColor,tableRow[row].children[col+1].style.backgroundColor, 
+                                tableRow[row].children[col+2].style.backgroundColor, tableRow[row].children[col+3].style.backgroundColor)){
+               return true;
+           }
+        }
     }
 }
 
+function verticalCheck(){
+    for (let col = 0; col < 7; col++){
+        for (let row = 0; row < 3; row++){
+            if (colorMatchCheck(tableRow[row].children[col].style.backgroundColor, tableRow[row+1].children[col].style.backgroundColor,
+                                tableRow[row+2].children[col].style.backgroundColor,tableRow[row+3].children[col].style.backgroundColor)){
+                return true;
+            };
+        }   
+    }
+}
+
+function diagonalCheck(){
+    for(let col = 0; col < 4; col++){
+        for (let row = 0; row < 3; row++){
+            if (colorMatchCheck(tableRow[row].children[col].style.backgroundColor, tableRow[row+1].children[col+1].style.backgroundColor,
+                tableRow[row+2].children[col+2].style.backgroundColor,tableRow[row+3].children[col+3].style.backgroundColor)){
+                    return true;
+                }
+            }
+        }
+
+}
+
+function diagonalCheck2(){
+    for(let col = 0; col < 4; col++){
+        for (let row = 5; row > 2; row--){
+            if (colorMatchCheck(tableRow[row].children[col].style.backgroundColor, tableRow[row-1].children[col+1].style.backgroundColor,
+                tableRow[row-2].children[col+2].style.backgroundColor,tableRow[row-3].children[col+3].style.backgroundColor)){
+                    return true;
+            }
+        }
+    }
+}
+
+function drawCheck(){
+    let fullSlot = []
+    for (i=0; i < tableData.length; i++){
+        if (tableData[i].style.backgroundColor !== 'white'){
+            fullSlot.push(tableData[i]);
+        }
+    }
+    if (fullSlot.length === tableData.length){
+        return true;
+    }
+}
+
+restartBtn.addEventListener('click', () => {
+    spots.forEach(spot => {
+        spot.style.backgroundColor = 'white';
+    });
+    playerTurn.style.color = 'black';
+    return (currentPlayer === 1 ? playerTurn.textContent = `${player1}'s turn` : playerTurn.textContent = `${player2}'s turn`);
+});
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Connect-four-project-1-
+//Connect-four-project-1:
+You read it correctly, you'll be playing Connect Four!
+This is a tow player game that will ask for the name of each player and keep track of turns
+player 1 is orange, player 2 is blue
+the first to connect four is the winner! Have fun and play away!//
+
+//Technologies used// Css, html, javascript//
+//
+
+//planned future enhancements: Will be updating some javascript functions and adding animations to give the game the aesthetic of 'Dragonball Z'! images have been collected to use for styling, just need to properly format to have them fit within the game structure.//
+
+
+# project-1-assessment
